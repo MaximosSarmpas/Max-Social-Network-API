4099e7c25725dd694758bdbe3dcd6845c9404abf
Maximos Sarmpas <117605647+MaximosSarmpas@users.noreply.github.com>
2023-04-27 13:12:21 -0400

    Update connection.js
    comments added

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
+// Import the Mongoose library
 const mongoose = require('mongoose');
 
+// Enable strict mode for queries (enforces strict schema checks for query fields)
 mongoose.set('strictQuery', true);
 
-// Wrap Mongoose around local connection to MongoDB
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Export connection 
+// Export the connection object so it can be used by other modules
 module.exports = mongoose.connection;
