087be82a1e5a7ca8c0f18aa0b1312e1795279480
Maximos Sarmpas <117605647+MaximosSarmpas@users.noreply.github.com>
2023-04-27 13:57:25 -0400

    Update server.js
    comments added

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
+// Import the express library
 const express = require('express');
+
+// Import the database configuration from the 'config' folder
 const db = require('./config/connection');
+
+// Import the API routes from the 'routes' folder
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Use the express middleware to parse incoming URL-encoded data
 app.use(express.urlencoded({ extended: true }));
+
+// Use the express middleware to parse incoming JSON data
 app.use(express.json());
+
+// Use the imported routes for handling API requests
 app.use(routes);
 
-// Check for successful connection in connection.js
+// Connect to the database and start the server once the connection is open
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+  
+  // Start the express server and listen for incoming requests on the specified port
+app.listen(PORT, () => {
+   
+  // Log a message indicating that the server is running and on which port
+ console.log(`API server running on port ${PORT}!`);
   });
 });
