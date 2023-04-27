53c9df50cdf95ddcee2457cca42f9040c6c325a5
Maximos Sarmpas <117605647+MaximosSarmpas@users.noreply.github.com>
2023-04-27 13:13:13 -0400

    Update connection.js
    comments added

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 // Enable strict mode for queries (enforces strict schema checks for query fields)
 mongoose.set('strictQuery', true);
 
-
+// Connect to MongoDB using the provided URI or fallback to a local MongoDB instance
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
